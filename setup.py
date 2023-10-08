96b1fcffc126b553b6fb9ab1c1f75ec11e4bbc60
isouriadakis <70639771+isouriadakis@users.noreply.github.com>
2023-10-08 19:14:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 import numpy as np
 from Cython.Build import cythonize
 
-include_path = [np.get_include()]
+include_path = [np.get_include(),'/usr/local/lib','/usr/local/include/embree3/']
 
 ext_modules = cythonize('pyembree/*.pyx', language='c++',
                         include_path=include_path)
 for ext in ext_modules:
     ext.include_dirs = include_path
-    ext.libraries = ["embree"]
+    ext.libraries = ["embree3"]
 
 setup(
-    name="pyembree3",
+    name="pyembree",
     version='0.2.0',
     ext_modules=ext_modules,
     zip_safe=False,
